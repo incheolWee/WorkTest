854b4c71d378b05186fd38a8be8944c5b23245fb
dnldl <dnldlscjf@gmail.com>
2024-09-03 03:01:09 +0900

    파일 생성 테스트

---
+++ src/test/java/com/example/worktest/WorkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
