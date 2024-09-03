2f56a9e57786930628df100205c156d2c237452d
dnldl <dnldlscjf@gmail.com>
2024-09-03 17:18:20 +0900

    WorkRepository 생성

---
+++ src/main/java/com/example/worktest/Work/WorkRepository.java
@@ -0,0 +1,11 @@
+package com.example.worktest.Work;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkRepository extends JpaRepository<Work, Long> {
+
+}
+
+
